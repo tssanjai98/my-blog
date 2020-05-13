d89ecb1f7f8b48eec4bc980599fc53c820479c9a
tssanjai98 <tssanjai98@github.com>
2020-05-13 13:27:43 +0530

    First commit

---
+++ src/build/precache-manifest.715ec8140cd0424e4f3ec2de10e76766.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e8d1d55fcceabc5fc32",
+    "url": "/static/css/main.810405b0.chunk.css"
+  },
+  {
+    "revision": "9e8d1d55fcceabc5fc32",
+    "url": "/static/js/main.b60acd2f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a7d7cfe024495ebc414e",
+    "url": "/static/js/2.59ab51fc.chunk.js"
+  },
+  {
+    "revision": "3d555aca86fbf2435696db2f2e408e90",
+    "url": "/index.html"
+  }
+];
